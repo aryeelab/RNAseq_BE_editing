edbefa5cafa8ad9f1185e0fe37d378efa1244f58
Sowmya Iyer <sowmyai@bu.edu>
2019-05-20 11:31:56 -0400

    updated to add missing step of creating poskey before sorting and joining

--- code/step2.sh
+++ code/step2.sh
@@ -49,18 +49,16 @@ READCOUNTS_TREATED=`echo ${READCOUNTS_TREATED_GZIP} | sed 's/\.gz//g'`
 #zcat ${READCOUNTS_TREATED_GZIP} > ${READCOUNTS_TREATED}
 
 # Sort files to be joined for control. Join is done by position key chr_position, which is first column
-LC_ALL=C zcat ${READCOUNTS_CONTROL_GZIP} | sort -k1,1 > ${READCOUNTS_CONTROL}.sorted
-LC_ALL=C sort -k1,1 ${VCF_CONTROL} >  ${VCF_CONTROL}.sorted 
-LC_ALL=C join ${VCF_CONTROL}.sorted ${READCOUNTS_CONTROL}.sorted -1 1 -2 1 | sed 's/ /\t/g' | cut -f1-7,14-21 > ${VCF_CONTROL}.sorted.vcf_with_coverage.txt 
-		
+LC_ALL=C zcat ${READCOUNTS_CONTROL_GZIP} | awk '{ print $1"_"$2"\t"$0}'  | sort -k1,1 > ${READCOUNTS_CONTROL}.sorted
+LC_ALL=C awk '{print $1"_"$2"\t"$0}' ${VCF_CONTROL} | sort -k1,1 >  ${VCF_CONTROL}.sorted 
+join ${VCF_CONTROL}.sorted ${READCOUNTS_CONTROL}.sorted -1 1 -2 1 | sed 's/ /\t/g' | cut -f1-7,14-21 > ${VCF_CONTROL}.sorted.vcf_with_coverage.txt 
 
 # Sort files to be joined for treated. Join is done by position key chr_position, which is first column
-LC_ALL=C zcat ${READCOUNTS_TREATED_GZIP} | sort -k1,1 > ${READCOUNTS_TREATED}.sorted 
-LC_ALL=C sort -k1,1 ${VCF_TREATED} > ${VCF_TREATED}.sorted 
-LC_ALL=C join ${VCF_TREATED}.sorted ${READCOUNTS_TREATED}.sorted -1 1 -2 1 | sed 's/ /\t/g' | cut -f1-7,14-21 > ${VCF_TREATED}.sorted.vcf_with_coverage.txt 
-		
+LC_ALL=C zcat ${READCOUNTS_TREATED_GZIP} | awk '{print $1"_"$2"\t"$0}' | sort -k1,1 > ${READCOUNTS_TREATED}.sorted 
+LC_ALL=C awk '{print $1"_"$2"\t"$0}' ${VCF_TREATED} | sort -k1,1 > ${VCF_TREATED}.sorted 
+join ${VCF_TREATED}.sorted ${READCOUNTS_TREATED}.sorted -1 1 -2 1 | sed 's/ /\t/g' | cut -f1-7,14-21  > ${VCF_TREATED}.sorted.vcf_with_coverage.txt 
 
 # Join treated vcf B (that now has treated bam-readcounts)  with control bam read counts. Fill with 0 for entries where the treated vcf does not have a corresponding entry in control bam-readcounts
-LC_ALL=C join ${VCF_TREATED}.sorted.vcf_with_coverage.txt ${READCOUNTS_CONTROL}.sorted -1 1 -2 1 -a 1 | sed 's/ /\t/g' | awk '{ if (NF == 14) {print $0"\t0\t0\t0\t0\t0\t0\t0\t0\t0"} else {print $0}}' > ${VCF_TREATED}.sorted.vcf_with_coverage.be_and_control.txt 
+join ${VCF_TREATED}.sorted.vcf_with_coverage.txt ${READCOUNTS_CONTROL}.sorted -1 1 -2 1 -a 1 | sed 's/ /\t/g' | awk '{ if (NF == 14) {print $0"\t0\t0\t0\t0\t0\t0\t0\t0\t0"} else {print $0}}' > ${VCF_TREATED}.sorted.vcf_with_coverage.be_and_control.txt
 
-
+#Rscript step3.R ${VCF_TREATED}.sorted.vcf_with_coverage.be_and_control.txt
